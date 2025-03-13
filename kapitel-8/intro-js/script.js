119a4f7739a4dfb73b76b3717181aeb1c5a2f10e
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2025-03-13 14:26:22 +0100

    tränat på java script, variabeler. konkatenering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,21 @@
+
+
+// var tal1 = 12;
+// var tal2 = 34
+// var summa = tal1 + tal2;
+// alert(tal1 + " + "  + tal2 + " = " + summa);
+
+var förnamn = "jasmine";
+var efternamn = "Lindberg";
+var namn = förnman + "  "  +efternamn;
+var ålder = 18;
+
+// var meddelande = namn + " är " + ålder + " år gammal ";
+
+// alert(meddelande)
+
+var hobby = "jobba"
+var kostar = "gratis"
+var meddelande = namn + " gillar " + hobby + " /n och är " + kostar + " per år";
+alert(meddelande);
+
