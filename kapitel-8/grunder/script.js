997335a65adbae841d06102eb6462757aa55f588
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2025-03-21 09:44:16 +0100

    tränat på if-statser och loopar

---
+++ kapitel-8/grunder/script.js
@@ -0,0 +1,64 @@
+// här skriver vi Js-kod
+
+// var tal1 = parseInt (prompt("skriv in ett heltal:"));
+// var tal2 = parseInt(prompt("skriv in ett till heltal:"));
+
+// if (tal1 > tal2) {
+
+//     document.writeln(`${tal1} är större än ${tal2}`);
+
+// }
+
+// if (tal1 < tal2) {
+
+//     document.writeln(`${tal2} är större än eller lika med ${tal1}`);
+
+// }
+
+// En liten inloggning
+
+
+
+
+
+// while (true) {
+    
+//     var konto = (prompt("vad heter du?"));
+//     var lösenord = (prompt("skirv lösenord"));
+    
+
+// if (lösenord == 123) {
+
+//     document.writeln(`${konto} har blivit inloggad`);
+
+//     break;
+
+// }
+
+// if (lösenord == false) 
+// {
+
+//     document.writeln(`${konto} har skrivit fel lösenord`);
+
+// }
+
+
+// }
+
+// skriv ut talen 20-30 
+
+for (let index = 20; index < 31; index++) {
+
+    document.writeln(`tal: ${index} <br>`);
+    
+}
+
+let val= true;
+
+
+while(val) {
+
+    let val = confirm("klicka Ok för att forsätta");
+    break;
+
+}
