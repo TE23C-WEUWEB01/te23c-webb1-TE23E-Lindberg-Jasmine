a73867f2ea63d4b1a04abad9a2ac89c4153aeb99
jasmine lindberg <Jasmine.lindberg@elev.ga.ntig.se>
2025-04-10 14:27:20 +0200

    tränat på click-event,ändra stil, och dölja/visa text o bild

---
+++ kapitel-8/teoriprov/click-event/script.js
@@ -0,0 +1,42 @@
+function setDarkMode() {
+    
+    document.body.style = "background: #000; color: #fff;";
+
+}
+function setLightkMode() {
+    
+    document.body.style = "background: #fff; color: #00;";
+
+}
+
+function setFunnyMode() {
+
+
+document.querySelector("h1").style = "font-size:100px; color:#65d623; background: #000" ;
+
+
+    
+}
+
+function hide() {
+
+    document.querySelector("h1").style = "visibility: hidden;";
+    
+}
+
+function show() {
+
+    document.querySelector("h1").style = "visibility: visible;";
+    
+
+}
+function varning() {
+
+    document.querySelector("p").style = "visibility: visible; color:red;";
+    
+}
+function bild() {
+
+    document.querySelector("img").style = "visibility: visible; ";
+    
+}
